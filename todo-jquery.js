8b763cb8880fa826262dfc6986647d173e52b50d
Desmond Pringle <dezmondp@gmail.com>
2018-04-30 06:10:44 -0500

    Adds todo jQuery

---
+++ todo-jquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var list = $('#list');
+    var addTodo = $('#addTodo');
+
+    $('#submit').on('click', function(){
+        var newChkbx = $('<input type="checkbox">');
+        var newEl = $('<li>').text(addTodo.val());
+        newChkbx.prependTo(newEl);
+        newEl.appendTo(list);
+        // list.append(newEl);
+        addTodo.val('').focus();
+    });
+
+    $('ul').on('click', function(){
+        var chkbx = $('input:checkbox');
+        var chkbxArray = Array.from(chkbx);
+        chkbxArray.forEach(function(item){
+            parent = $(item).parent();
+            // console.log(parent)
+            if(item.checked){
+                parent.css("textDecoration", "line-through");
+                setTimeout(function(){
+                    console.log(parent)
+                    parent.remove();
+                }, 2000)
+            }else{
+                parent.css("textDecoration", "none")
+            }
+        })
+    })
+});
